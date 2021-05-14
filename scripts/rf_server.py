1293c9fd6b06531c012cc27daddb47c31ff89ae0
shroomcloudm <opschon@gmail.com>
2021-05-14 08:29:31 +0100

    remove duplicate

--- scripts/rf_server.py
+++
@@ -1,6 +0,0 @@
-from robotremoteserver import RobotRemoteServer
-from keyboardDevice import keyboardDevice
-
-server = RobotRemoteServer(keyboardDevice(), host='', port=8270,
-                           port_file='/tmp/remote-port.txt', serve=False)
-server.serve()
